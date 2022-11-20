e87e57e6a4900fca42b0ab0587acf9d2ce2d821d
Jon Werber <jon.w@icontechstudio.com>
2022-11-19 19:50:08 -0500

    random color

---
+++ weirdo/scripts.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {myFunction()};
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+function getRandomColor(){
+    return  Math.floor(Math.random()*16777215).toString(16);
+}
+function myFunction() {
+    console.log('hit')
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    const body =   document.getElementById("text-overlay");
+    console.log('body',body)
+body.style.border = `50px solid #${getRandomColor()}`;
+}
+
